9818d5e4edc9b2ec64763fa2d52e29115127ecfb
harikesh <harikesh@ninjacart.com>
2023-07-05 15:22:35 +0530

    posthog integrated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import posthog from 'posthog-js';
+import { PostHogProvider} from 'posthog-js/react';
+
+posthog.init('phc_UmL7iU4VxAiIq9AExYqcI1yNuuxZSxzpQshFjVuLD3S', { api_host: 'https://posthog.ninjacart.in' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
 
